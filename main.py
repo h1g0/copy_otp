ed09ab6e54d4feb6caa3b0e36b1207d67b536c42
hi_go <me@clutte.red>
2021-06-24 02:55:47 +0900

    Refactor.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pyotp
 import pyperclip
 
+
 def get_totp(secret: str) -> str:
     totp = pyotp.TOTP(secret)
     result = totp.now()
     return result
 
-def get_skey_from_file(file_path: str)->str:
+
+def get_skey_from_file(file_path: str) -> str:
     with open(file_path) as f:
         result = f.readline().strip()
         return result
+
 
 if __name__ == '__main__':
     result = get_totp(get_skey_from_file('secret.txt'))
