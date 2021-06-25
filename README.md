b342767b8c69889260c9376be179df366a28428d
hi_go <me@clutte.red>
2021-06-25 19:35:41 +0900

    Change to store the secret securely.

--- README.md
+++ README.md
@@ -7,17 +7,14 @@ A simple Python script that generates Time-based One-time Password and copy it t
 1. Run
    ```sh
    pip install pyotp
+   pip install keyring
    ```
    to get the libraries required.
 2. Get your private key. (Please google how to get your private key!)
-3. Save it in the same directory as `main.py` with the name `secret.txt`.
-4. Run
+3. Run
    ```sh
    python main.py
    ```
    in a shell (or execute `copy_otp.bat` if you are using Windows).
+4. The first time you run the script, you will be asked to enter the secret key. Then input the key you got in step 2.
 5. Time-based One-time Password will be copied to the clipboard.
-
-## Attention
-
-This script stores the private key in plain text, which may lead to reduced security.
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from tabnanny import check
 import pyotp
+import keyring
 import pyperclip
+import re
 
+SERVICE_ID = 'COPY_OTP'
 
 def get_totp(secret: str) -> str:
     totp = pyotp.TOTP(secret)
@@ -8,13 +12,26 @@ def get_totp(secret: str) -> str:
     return result
 
 
-def get_skey_from_file(file_path: str) -> str:
-    with open(file_path) as f:
-        result = f.readline().strip()
-        return result
+def get_skey() -> str:
+    return keyring.get_password(SERVICE_ID,"totp")
 
+def set_skey(key: str):
+    keyring.set_password(SERVICE_ID,"totp",key)
+
+def check_is_base32(key:str):
+    return True if re.fullmatch('[A-Z2-7=]+',key) else False
 
 if __name__ == '__main__':
-    result = get_totp(get_skey_from_file('secret.txt'))
+    skey: str = get_skey()
+    #skey = ''
+    while not skey:
+        skey = input("Please input the secret for TOTP: ").strip()
+        if(check_is_base32(skey)):
+            set_skey(skey)
+        else:
+            print('Invalid secret! Input again: ')
+            skey = ''    
+
+    result = get_totp(skey)
     pyperclip.copy(result)
     print(f'OTP {result} copied!')
