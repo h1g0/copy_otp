da9fd0fd2e1144dba71fb664f11498210f1986fa
hi_go <me@clutte.red>
2021-06-26 02:27:15 +0900

    Implement reading QR code.

--- README.md
+++ README.md
@@ -2,19 +2,25 @@
 
 A simple Python script that generates Time-based One-time Password and copy it to clipboard.
 
+Reading the secret can be done both by reading the QR code or by entering a text string.
+
+The secret will be stored in encryption.
+
 ## Usage
 
 1. Run
    ```sh
    pip install pyotp
    pip install keyring
+   pip install pyzbar
    ```
    to get the libraries required.
-2. Get your private key. (Please google how to get your private key!)
-3. Run
+2. Get your QR code or secret for your 2FA. (Please google how to get your QR code or secret!)
+3. If you got your QR code, save the QR code you got in the same directory with the name `qr.png`.
+4. Run
    ```sh
    python main.py
    ```
    in a shell (or execute `copy_otp.bat` if you are using Windows).
-4. The first time you run the script, you will be asked to enter the secret key. Then input the key you got in step 2.
-5. Time-based One-time Password will be copied to the clipboard.
+5. The first time you run the script, if the image file of the QR code exists, the secret will be read from the QR code and the image file will be deleted. If not, you will be asked to enter the secret.
+6. Time-based One-time Password will be copied to the clipboard.
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from tabnanny import check
 import pyotp
 import keyring
+from pyzbar.pyzbar import decode
+from PIL import Image
 import pyperclip
 import re
+import os
 
 SERVICE_ID = 'COPY_OTP'
+
 
 def get_totp(secret: str) -> str:
     totp = pyotp.TOTP(secret)
@@ -13,24 +16,68 @@ def get_totp(secret: str) -> str:
 
 
 def get_skey() -> str:
-    return keyring.get_password(SERVICE_ID,"totp")
+    return keyring.get_password(SERVICE_ID, "totp")
+
 
 def set_skey(key: str):
-    keyring.set_password(SERVICE_ID,"totp",key)
+    keyring.set_password(SERVICE_ID, "totp", key)
 
-def check_is_base32(key:str):
-    return True if re.fullmatch('[A-Z2-7=]+',key) else False
 
-if __name__ == '__main__':
-    skey: str = get_skey()
-    #skey = ''
-    while not skey:
-        skey = input("Please input the secret for TOTP: ").strip()
-        if(check_is_base32(skey)):
-            set_skey(skey)
+def input_skey(repeat_until_valid: bool = True) -> str:
+    result = ''
+    while not result:
+        result = input("Please input the secret for TOTP: ").strip()
+        if(check_is_base32(result)):
+            return result
         else:
             print('Invalid secret! Input again: ')
-            skey = ''    
+            if(repeat_until_valid):
+                result = ''
+            else:
+                return result
+
+
+def parse_otpauth_uri(uri: str) -> dict:
+    # otpauth uri:
+    # ```
+    # otpauth://totp/[issuer]:[accountname]?secret=[secret]&issuer=[issuer]&algorithm=SHA1&digits=6&period=30
+    # ```
+    # cf. https://github.com/google/google-authenticator/wiki/Key-Uri-Format
+    result = {
+        'valid': False,
+        'secret': '',
+    }
+    if 'otpauth://totp/' in uri:
+        result['valid'] = True
+        result['secret'] = re.search(r'secret=([A-Z2-7=]+)', uri).groups()[0]
+        if(not result['secret']):
+            result['valid'] = False
+    else:
+        result['valid'] = False
+    return result
+
+
+def read_qr_image(file_path: str) -> str:
+    d = decode(Image.open(file_path))
+    return d[0].data.decode("utf-8")
+
+
+def check_is_base32(key: str) -> bool:
+    return True if re.fullmatch(r'[A-Z2-7=]+', key) else False
+
+
+if __name__ == '__main__':
+    qr_image = 'qr.png'
+    skey: str = get_skey()
+    if(not skey):
+        #skey = input_skey()
+        uri = parse_otpauth_uri(read_qr_image(qr_image))
+        if(uri['valid']):
+            skey = uri['secret']
+            os.remove(qr_image)
+        else:
+            print('Invalid QR code!')
+            skey = input_skey()
 
     result = get_totp(skey)
     pyperclip.copy(result)
